225b8002b948d95a4b60d54a75ac7393d86fbdeb
IRules <48042380+IRules@users.noreply.github.com>
2025-02-14 10:49:43 +0200

    Initial commit. Working prototype.

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,30 @@
+// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use std::fs;
+
+
+#[tauri::command]
+fn save_chart(elements_json: &str) {
+    let path = "chart.json";
+    fs::write
+    (
+        path,
+        elements_json
+    ).expect("Unable to write file");
+}
+
+#[tauri::command]
+fn load_chart() -> String {
+    let path = "chart.json";
+    let contents = fs::read_to_string(path)
+        .expect("Something went wrong reading the file");
+    return contents;
+}
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_opener::init())
+        .invoke_handler(tauri::generate_handler![save_chart, load_chart])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
---
+++ src/App.tsx
@@ -0,0 +1,78 @@
+import {Excalidraw, getSceneVersion} from "@excalidraw/excalidraw";
+
+import "./App.css";
+import {Toaster} from "./components/ui/sonner.tsx";
+import {toast} from "sonner"
+import {ExcalidrawElement} from "@excalidraw/excalidraw/types/element/types";
+import {invoke} from "@tauri-apps/api/core";
+import {useEffect, useState} from "react";
+import Element = React.JSX.Element;
+
+function App() {
+    let lastSceneVersion = 0
+
+    let lastChange = Date.now();
+
+    let resetTimeout = 5000;
+
+    function SaveOnChange(elements: readonly ExcalidrawElement[]) {
+        let sceneVersion = getSceneVersion(elements);
+
+        if (sceneVersion !== lastSceneVersion) {
+            lastChange = Date.now();
+
+            setTimeout(
+                () => {
+                    if (Date.now() - lastChange > resetTimeout) {
+                        let elements_json = JSON.stringify(elements, null, 2);
+
+                        toast("Saving to file...")
+
+                        invoke("save_chart", {elementsJson: elements_json}).then(() => {
+                            toast("Saved to file!")
+                        })
+                    }
+                },
+                resetTimeout
+            )
+
+            lastSceneVersion = sceneVersion;
+        }
+    }
+
+    function onRestore() {
+        invoke("load_chart").then((elements_json) => {
+            if (typeof elements_json !== "string") {
+                return
+            }
+            let elements: readonly ExcalidrawElement[] = JSON.parse(elements_json);
+            console.log(elements);
+            setElements(elements);
+            setExcalidrawElement(<Excalidraw theme={"dark"} initialData={{
+                elements
+            }} onChange={SaveOnChange}/>)
+        });
+    }
+
+    const [elements, setElements] = useState<readonly ExcalidrawElement[]>([]);
+
+    const [excalidraw_element, setExcalidrawElement] = useState<Element>(<></>);
+
+    useEffect(() => {
+        onRestore()
+        console.log("Elements: ", elements)
+    }, []);
+
+    return (
+        <>
+            <main className="container w-screen h-screen bg-black">
+                <div className="w-screen h-full">
+                    {excalidraw_element}
+                </div>
+            </main>
+            <Toaster/>
+        </>
+    );
+}
+
+export default App;
